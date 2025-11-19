401a6f5490b67885598eee89d3251d52b962c5f2
Daniela Coimbra Hitomi <daniela.hitomi@usp.br>
2025-11-19 14:44:07 -0300

    Edit 0001_initial.py

--- blog/migrations/0001_initial.py
+++ blog/migrations/0001_initial.py
@@ -13,6 +13,13 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
+         migrations.CreateModel(
+            name='Category',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('name', models.CharField(max_length=100, unique=True)),
+            ],
+        ),
 
         migrations.CreateModel(
             name='Post',
